2adfb2c2070f8bcfce06bd0022d3901017dfcd7f
aminul-islam-niloy <niloyftaminul@gmail.com>
2024-08-10 01:44:37 +0600

    Package install and model create

---
+++ Tripple Angular/Models/Book.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Tripple_Angular.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+
+        [Required]
+        [StringLength(50)]
+        public string Author { get; set; }
+
+        public int YearPublished { get; set; }
+
+        [Range(0, 1000)]
+        public decimal Price { get; set; }
+    }
+}
